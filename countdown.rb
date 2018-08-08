dbfbc6bb2c3268d63fa6877c5142b8681e4c70f5
natashabhuta <natasha.bhuta@gmail.com>
2018-08-08 19:29:55 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,4 +9,11 @@ end
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep( )
+def countdown_with_sleep(seconds)
+  while seconds > 0
+  sleep 1 
+ puts "#{seconds} SECOND(S)"
+  seconds -= 1 
+end 
+  return "HAPPY NEW YEAR!"
+end
